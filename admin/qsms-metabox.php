210acce0ac74b8383f842751cb067426d89974a5
Shah jalal <rjshahjalal132@gmail.com>
2024-08-01 20:19:48 +0600

    added required fields for qata_message post type

--- admin/qsms-metabox.php
+++ admin/qsms-metabox.php
@@ -10,6 +10,50 @@ if ( class_exists( 'CSF' ) ) {
         'title'        => 'Message',
         'post_type'    => 'qata_message',
         'show_restore' => true,
-      ) );
+    ) );
+
+    CSF::createSection( $prefix, array(
+        'title'  => 'Message',
+        'icon'   => '',
+        'fields' => array(
+
+            // Status field
+            array(
+                'id'          => 'qsms_order_status',
+                'type'        => 'select',
+                'title'       => 'Status',
+                'placeholder' => 'Select an Status',
+                'options'     => array(
+                    'processing' => 'Processing',
+                    'hold'       => 'On Hold',
+                    'cancelled'  => 'Cancelled',
+                ),
+            ),
+
+            // Template code field
+            array(
+                'id'          => 'qsms_template_code',
+                'type'        => 'text',
+                'title'       => 'Template Code',
+                'placeholder' => 'Template Code',
+            ),
+
+            // Repeater field
+            array(
+                'id'     => 'qsms_params',
+                'type'   => 'repeater',
+                'title'  => 'Parameters',
+                'fields' => array(
+                    array(
+                        'id'    => 'qsms_param',
+                        'type'  => 'text',
+                        'title' => 'Param',
+                    ),
+
+                ),
+            ),
+
+        ),
+    ) );
 
 }
