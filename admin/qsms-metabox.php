c6c7d5a6c42ea400d8fe3a6d6d640f892d806737
Shah jalal <rjshahjalal132@gmail.com>
2024-08-01 23:37:56 +0600

    replace static statuses to dynamic order statuses

--- admin/qsms-metabox.php
+++ admin/qsms-metabox.php
@@ -7,7 +7,9 @@ if ( !defined( 'ABSPATH' ) ) {
 if ( class_exists( 'CSF' ) ) {
 
     // Get WooCommerce order statuses
-    $order_statuses = get_option( '_wc_order_statuses' ) ?? '';
+    $order_statuses = get_option( '_wc_order_statuses' ) ?? array();
+    // Decode to array
+    $order_statuses = json_decode( $order_statuses );
 
     // Prefix
     $prefix = '_qata_message';
@@ -19,6 +21,14 @@ if ( class_exists( 'CSF' ) ) {
         'show_restore' => true,
     ) );
 
+    // Convert order statuses to a format suitable for dropdown
+    $status_options = array();
+    foreach ( $order_statuses as $status_key => $status_label ) {
+        $status_key                  = str_replace( 'wc-', '', $status_key ); // Remove 'wc-' prefix
+        $status_label                = translate( $status_label );
+        $status_options[$status_key] = $status_label;
+    }
+
     CSF::createSection( $prefix, array(
         'title'  => 'Message',
         'icon'   => '',
@@ -29,12 +39,8 @@ if ( class_exists( 'CSF' ) ) {
                 'id'          => 'qsms_order_status',
                 'type'        => 'select',
                 'title'       => 'Status',
-                'placeholder' => 'Select an Status',
-                'options'     => array(
-                    'processing' => 'Processing',
-                    'hold'       => 'On Hold',
-                    'cancelled'  => 'Cancelled',
-                ),
+                'placeholder' => 'Select a Status',
+                'options'     => $status_options,
             ),
 
             // Template code field
@@ -61,42 +67,13 @@ if ( class_exists( 'CSF' ) ) {
                         'id'          => 'qsms_param_value',
                         'type'        => 'select',
                         'title'       => 'Parameter Value',
-                        'placeholder' => 'Select an Value',
-                        'options'     => array(
-                            'processing' => 'Processing',
-                            'hold'       => 'On Hold',
-                            'cancelled'  => 'Cancelled',
-                        ),
+                        'placeholder' => 'Select a Value',
+                        'options'     => $status_options,
                     ),
-
                 ),
             ),
 
         ),
     ) );
 
-}
-
-function put_api_response_data( $data ) {
-    // Ensure directory exists to store response data
-    $directory = QATA_MESSAGE_PLUGIN_PATH . '/api_response/';
-    if ( !file_exists( $directory ) ) {
-        mkdir( $directory, 0777, true );
-    }
-
-    // Construct file path for response data
-    $fileName = $directory . 'response.log';
-
-    // Get the current date and time
-    $current_datetime = date( 'Y-m-d H:i:s' );
-
-    // Append current date and time to the response data
-    $data = $data . ' - ' . $current_datetime;
-
-    // Append new response data to the existing file
-    if ( file_put_contents( $fileName, $data . "\n\n", FILE_APPEND | LOCK_EX ) !== false ) {
-        return "Data appended to file successfully.";
-    } else {
-        return "Failed to append data to file.";
-    }
 }
